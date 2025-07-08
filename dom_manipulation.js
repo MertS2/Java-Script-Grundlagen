d27f063470ebe94e8b7ed4434eaeb28ca6f9fcae
MertS2 <soenmezmert441@gmail.com>
2025-07-08 07:31:28 +0200

    updated

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -17,5 +17,32 @@ Damit kann man Inhalt einer HTML Elemente verändern und Ausgeben.
 Hinweis, da Javascript von oben nach unten lies muss die verlinkt der datei unter dem </body> sein, anstonsten kommt die Meldung null heraus.   
 
 
+
 */
 
+document.getElementById('test_div').classList.add('blue_bg');
+document.getElementById('test_div_2').classList.remove('yellow_bg');
+document.getElementById('test_div_2').classList.toggle('yellow_bg');
+
+/* 
+classList.add: class hinzufügen.
+classList.remove: class entfernen.
+
+classList.toggle: Kombination aus add und remove, wenn die classe in HTML drinnen ist nehmt er es weg, wenn es nicht drinnen ist fügt er es hinzu. 
+*/
+
+
+
+
+/* Atribut ändern: */
+
+document.getElementById('inputfield_test').setAttribute('type', 'text');
+document.getElementById('inputfield_test').setAttribute('value', 'Einfügen');
+
+/* Value in der Konsole anzeigen: */
+
+console.log(document.getElementById('inputfield_test').value);
+
+
+
+/* Mit set Atribut kann man die Atribute ändern, wie z,b die ID oder den typ von einem input usw. */
