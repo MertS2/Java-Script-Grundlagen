4c3a8fbbedb47b0d86e7b6a1f6fb08974be7ccf5
MertS2 <soenmezmert441@gmail.com>
2025-07-24 17:49:03 +0200

    updated

---
+++ Fotogram/script.js
@@ -0,0 +1,18 @@
+let imgs = [
+    "belgium-4966646_1280.jpg",
+    "berlin - 61994_1280.jpg",
+    "copenhagen - 4746327_1280.jpg",
+    "galata - 4043037_1280.jpg",
+    "las - vegas - 2722762_1280.jpg",
+    "linz - 3743776_1280.jpg",
+    "los - angeles - 3580722_1280.jpg",
+    "mallorca - 3690605_1280.jpg",
+    "paris - 1726315_1280.jpg",
+    "singapore - 1927720_1280.jpg",
+    "stuttgart - 68760_1280.jpg",
+    "switzerland - 7899656_1280.jpg",
+]
+
+function init () {
+    
+}
--- übungen/07_Aufgabe_for_schleife/script.js
+++ übungen/07_Aufgabe_for_schleife/script.js
@@ -1,7 +1,16 @@
-Aufgabenstellung 1: Summe der Elemente eines Arrays
+/* Aufgabenstellung 1: Summe der Elemente eines Arrays
 Implementiere eine for-Schleife, um die Summe der Elemente in einem Array zu berechnen und zurückzugeben.
 
-    Test:
+    Test: */
+function sumArray(number) {
+    let sum = 0;
+    for (let i = 0; i < number.length; i++) {
+        sum += number[i];
+    }
+    return sum;
+};
+
+
 
 console.log(sumArray([3, 7, 1, 4])); // Erwartete Ausgabe: 15 (3 + 7 + 1 + 4)
 
@@ -10,10 +19,17 @@ console.log(sumArray([1, 2, 3, 4, 5])); // Erwartete Ausgabe: 15 (1 + 2 + 3 + 4 
 
 
 
-Aufgabenstellung 2: Ausgabe einer Zahlenreihe
+/* Aufgabenstellung 2: Ausgabe einer Zahlenreihe
 Implementiere eine for-Schleife, um die Zahlen von 1 bis n in aufsteigender Reihenfolge auszugeben.
 
-    Test:
+    Test: */
+function printNumbers(number) {
+    for (let i = 1; i < number + 1; i++) {
+        console.log(i);
+    }
+}
+
+
 
 printNumbers(5); // Erwartete Ausgabe: 1, 2, 3, 4, 5
 
@@ -22,11 +38,18 @@ printNumbers(3); // Erwartete Ausgabe: 1, 2, 3
 
 
 
-Aufgabenstellung 3: Rückwärts laufende for-Schleife
+/* Aufgabenstellung 3: Rückwärts laufende for-Schleife
 Aufgabe:
 
 Implementiere eine for-Schleife, die die Zahlen von der gegebenen Zahl bis 1 in absteigender Reihenfolge ausgibt.
-    Test:
+    Test: */
+function printNumbersReverse(number) {
+    for (let i = number; i > + 0; i--) {
+        console.log(i);
+    }
+}
+
+
 
 printNumbersReverse(5);
 
@@ -38,10 +61,17 @@ printNumbersReverse(3);
 // Erwartete Ausgabe: 3, 2, 1
 
 
-Aufgabenstellung 4: Nur jedes dritte Element ausgeben
+/* Aufgabenstellung 4: Nur jedes dritte Element ausgeben
 Implementiere eine for-Schleife, die jedes dritte Element des gegebenen Arrays ausgibt.
 
-    Test:
+    Test: */
+
+function printEveryThirdElement(number) {
+    for (let i = 0; i < number.length; i = i + 3) {
+        console.log(number[i]);
+    }
+}
+
 
 printEveryThirdElement([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
@@ -54,10 +84,19 @@ printEveryThirdElement(['a', 'b', 'c', 'd', 'e', 'f']);
 
 
 
-Bonusaufgabe: Prüfung auf Primzahlen
+/* Bonusaufgabe: Prüfung auf Primzahlen
 Implementiere eine for-Schleife, um zu überprüfen, ob die gegebene Zahl eine Primzahl ist(d.h.nur durch 1 und sich selbst teilbar ist).Gib true zurück, wenn sie eine Primzahl ist, andernfalls false.
 
-    Test:
+    Test: */
+
+function isPrime(number) {
+    for (i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 console.log(isPrime(7)); // Erwartete Ausgabe: true (7 ist eine Primzahl)
 
