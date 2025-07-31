f48dd5148ec97d958eb436439eae8b7fd338dc6c
MertS2 <soenmezmert441@gmail.com>
2025-07-31 21:59:45 +0200

    updated

--- Fotogram/script.js
+++ Fotogram/script.js
@@ -11,6 +11,9 @@ let imgs = [
     "./assets/singapore-1927720_1280.jpg",
     "./assets/stuttgart-68760_1280.jpg",
     "./assets/switzerland-7899656_1280.jpg",
+    "./assets/antalya-7572191_1280.jpg",
+    "./assets/dubai-1227538_1280.jpg",
+    "./assets/sea-984944_1280.jpg",
 ]
 
 let headlines = [
@@ -26,6 +29,10 @@ let headlines = [
     "Singapure",
     "Stuttgart",
     "Schweiz",
+    "Antalya",
+    "Dubai",
+    "Izmir"
+
 ]
 
 let description = [
@@ -47,14 +54,14 @@ let description = [
 
 function init() {
     render();
+    loadHeader();
+    loadFooter();
 }
 
 function render() {
     let main_inhalt = document.getElementById("main");
-    let main_headline = document.getElementById("main");
 
     main_inhalt.innerHTML = "";
-    main_headline.innerHTML = "";
 
     for (let i = 0; i < imgs.length; i++) {
         main_inhalt.innerHTML += template(headlines[i], imgs[i], description[i]);
@@ -65,22 +72,78 @@ function render() {
 
 function template(path, path2, path3) {
     return `   
-   <dialog>
+
+    <article>
+
+    <header>
+        <h2>${path}</h2>
+    </header>
+
+    <figure>
+        <figcaption>
+            <p> Figcaption </p>
+        </figcaption>
+
+        <img class="imgs_container_main" src="${path2}">
         
-    <h2>${path}</h2>
+        </figure>
 
-  
-    <img src="${path2}">;
+        <p>${path3}</p>
 
-    <p>${path3}</p>
-</dialog>
+    </article>
     `
 }
 
 
+function loadHeader() {
+    let header = document.getElementById("header"); /* HTML */
+    header.classList.add("header_main"); /* CSS */
+    header.innerHTML += template_header();
+}
+
+function loadFooter() {
+    let footer = document.getElementById("footer");
+    footer.classList.add("fotter_main");
+    footer.innerHTML += template_footer();
+}
+
 /* Responsive Menü leiste. */
+
 function toogle_rest_menu() {
     document.getElementById("resp_menu").classList.toggle("resp_menu_closed");
 }
 
+function template_header() {
+    return `
+        
+        <header class="header_main">
+
+        <img id="fotogram_logo" src="./icons/Fotogram_Logo.svg" alt="Fotogram_Logo">
+        <h1>Fotogram</h1>
+
+        <nav class="dektop_nav">
+            <a class="nav_link" href="./impresum.html">Impresum</a>
+            <a class="nav_link" href="">Datenschutz</a>
+            <a class="nav_link" href="">Kontakt</a>
+        </nav>
+
+        <button class="d_none" onclick="toogle_rest_menu()">
+            <img class="img_button" src="./icons/bars-solid.svg" alt="">
+        </button>
+    </header>
+
+    <nav id="resp_menu" class=" resp_menu_box resp_menu_closed">
+        <a class="nav_link" href="./impresum.html">Impresum</a>
+        <a class="nav_link" href="">Datenschutz</a>
+        <a class="nav_link" href="">Kontakt</a>
+    </nav>`
+}
+
+function template_footer() {
+    return `    
+
+    <footer>
+        <h3> © 2025 Mert Sönmez.</h3>
+    </footer>`
+}
 
