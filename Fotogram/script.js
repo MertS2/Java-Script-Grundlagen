61fb0a49d555fd26f69422493cbee4c2f5f9449d
MertS2 <soenmezmert441@gmail.com>
2025-08-14 19:59:22 +0200

    updated

--- Fotogram/script.js
+++ Fotogram/script.js
@@ -16,7 +16,9 @@ let imgs = [
     "./assets/antalya-7572191_1280.jpg",
     "./assets/dubai-1227538_1280.jpg",
     "./assets/sea-984944_1280.jpg",
-    "./assets/alanya-3758816_1280.jpg"
+    "./assets/alanya-3758816_1280.jpg",
+    "./assets/colosseum-5773684_1280.jpg",
+    "./assets/cyprus-7692597_1280.jpg",
 ]
 
 let headlines = [
@@ -36,9 +38,13 @@ let headlines = [
     "Dubai",
     "Izmir",
     "Alanya",
+    "Zypern",
+    "Rom"
 ]
 
 let show_more = [
+    "show more",
+    "show more",
     "show more",
     "show more",
     "show more",
@@ -114,6 +120,35 @@ let description = [
     `   Alanya, die sonnenverwöhnte Perle an der Türkischen Riviera, vereint mediterrane Gelassenheit mit historischem Charme.
         Vom majestätischen Burgberg mit Blick auf die azurblaue Küste bis zu endlosen Sandstränden – Alanya begeistert mit malerischer Natur, 
         lebendiger Kultur und herzlicher türkischer Gastfreundschaft.`,
+
+    `Zypern, die sonnenverwöhnte Insel im östlichen Mittelmeer, vereint mediterrane Lebensfreude mit einer faszinierenden Mischung aus Kulturen. 
+        Von goldenen Stränden und türkisblauem Meer bis zu uralten Ruinen und charmanten Bergdörfern – Zypern verzaubert mit abwechslungsreicher Natur, 
+        reicher Geschichte und der warmherzigen Gastfreundschaft seiner Menschen.`,
+
+    `Rom, die ewige Stadt am Tiber, vereint jahrtausendealte Geschichte mit italienischem Lebensgefühl. Zwischen majestätischen Bauwerken wie dem Kolosseum und dem Petersdom, 
+        charmanten Piazzas und belebten Gassen entfaltet sich ein einzigartiges Flair aus Kultur, Genuss und Dolce Vita – ein Ort, an dem Vergangenheit und Gegenwart harmonisch 
+        verschmelzen.`,
+];
+
+let site = [
+    "https://de.wikipedia.org/wiki/Belgien",
+    "https://de.wikipedia.org/wiki/Berlin",
+    "https://de.wikipedia.org/wiki/Kopenhagen",
+    "https://de.wikipedia.org/wiki/Istanbul",
+    "https://de.wikipedia.org/wiki/Las_Vegas",
+    "https://de.wikipedia.org/wiki/Las_Vegas",
+    "https://de.wikipedia.org/wiki/Los_Angeles",
+    "https://de.wikipedia.org/wiki/Mallorca",
+    "https://de.wikipedia.org/wiki/Paris",
+    "https://de.wikipedia.org/wiki/Singapur",
+    "https://de.wikipedia.org/wiki/Stuttgart",
+    "https://de.wikipedia.org/wiki/Schweiz",
+    "https://de.wikipedia.org/wiki/Antalya",
+    "https://de.wikipedia.org/wiki/Dubai",
+    "https://de.wikipedia.org/wiki/Izmir",
+    "https://de.wikipedia.org/wiki/Alanya",
+    "https://de.wikipedia.org/wiki/Zypern",
+    "https://de.wikipedia.org/wiki/Rom",
 ];
 
 
@@ -146,7 +181,6 @@ function template(path, path2, path3, i) {
     return `   
 
     <article>
-
     <header>
         <h2>${path}</h2>
     </header>
@@ -155,9 +189,7 @@ function template(path, path2, path3, i) {
         <img onclick="addOverlay(${i})" class="imgs_container_main" src="${path2}">
         
     </figure>
-
-        <button class="show_more_btn" >${path3}</button>
-
+        <button id="show_more_btn" onclick="showMoreDialog()" class="show_more_btn">${path3}</button>
     </article>
     `
 }
@@ -166,9 +198,9 @@ function template(path, path2, path3, i) {
 function dialog(path, path2, path3, i) {
     return `   
 
-    <dialog id="dialog" class="dialog" >
+    <dialog id="dialog" class="dialog">
 
-    <header class="dialog_header" >
+    <header class="dialog_header">
         <h2>${path}</h2>
         <button class="close_button" onclick="removeOverlay()">X</button>
     </header>
@@ -183,15 +215,47 @@ function dialog(path, path2, path3, i) {
         <button id="arrow_right_btn" onclick="arrowRight(${i})" class="dialog_arrow_btn"> => </button>
 
         </main>
-
+        <div class="dialog_footer">
         <p class="description_dialog">${path3}</p>
-
+        
+        <p class="counter_number">${i + 1}/18</p>
+        </div>
   </dialog>
   `
     /* 
     <button id = "arrow_right_btn_disabled" class="dialog_arrow_btn" style = "opacity : 0; pointer-events: none" disabled > <= </button >
     <button id="arrow_left_btn_disabled" class="dialog_arrow_btn" style="opacity : 0; pointer-events: none" disabled> <= </button>
     */
+}
+
+function showMoreTemplate() {
+    return `
+    <dialog class="show_more_dialog" id="show_more_container">
+    <header>
+    <button class="close_button" onclick="removeOverlay()">X</button>
+    </header>
+    <main>
+
+    <h5> Achtung, dieser Link geht auf einer externe Seite. </h5>
+    <h5> Klicken Sie auf OK, wenn sie einverstanden sind, wenn nicht schließen sie den Dialog über den X Button</h5>
+   
+    <button>Weiter</button>
+    </dialog>`
+
+
+}
+
+function showMoreDialog() {
+    let show = document.getElementById('dialog_container');
+    show.innerHTML = showMoreTemplate();
+
+    let dialogElement = document.getElementById('show_more_container');
+
+
+    if (dialogElement) {
+        dialogElement.showModal();
+    }
+
 }
 
 
@@ -202,6 +266,17 @@ function addOverlay(i) {
     /*     Leert den vorherigen Inhal*/
     refAddOverlay.innerHTML = "";
 
+    if (i == -1) {
+        addOverlay(i = 17);
+        refAddOverlay.innerHTML = "";
+    }
+
+    if (i == 18) {
+        addOverlay(i = 0);
+        refAddOverlay.innerHTML = "";
+    }
+
+
     /*     Fügt den neuen Dialog mit den entsprechenden Inhalten ein*/
     refAddOverlay.innerHTML += dialog(headlines[i], imgs[i], description[i], i); /* Fügt ins HTML die function mit den Parametern.  */
 
@@ -211,17 +286,11 @@ function addOverlay(i) {
         dialogElement.showModal();
     }
 
-    if (i == 0) {
-        dialog(i = 15);
-        dialog.innerHTML = "";
-        dialog.innerHTML += dialog(headlines[i], imgs[i], description[i], i)
-    }
-
-    if (i == 15) {
-        dialog(i = 1);
-        dialog.innerHTML = "";
-        dialog.innerHTML += dialog(headlines[i], imgs[i], description[i], i)
-    }
+    /*    if (refAddOverlay) {
+           document.body.classList.add("hidden");
+       } else {
+           document.body.classList.remove("hidden");
+       } */
 
 
     /* if (i == 0) { Entfernt den linken button in der Index 0. 
@@ -271,6 +340,7 @@ function arrowRight(i) {
     addOverlay(i + 1);
     addOverlay.innerHTML += dialog(headlines[i], imgs[i], description[i], i); /* Fügt ins HTML die function mit den Parametern.  */
 }
+
 
 /* Responsive Menü leiste. */
 
