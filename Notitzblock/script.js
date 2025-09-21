356ae49cb7e9c6109e6a1550e2bffd8c98cac0e8
MertS2 <soenmezmert441@gmail.com>
2025-09-21 15:54:21 +0200

    updated

--- Notitzblock/script.js
+++ Notitzblock/script.js
@@ -1,12 +1,12 @@
 let notes = ['UI überarbeiten', ' Reponsive Design überarbeiten',];
-let notesTitle = ['Task 1', 'Task 2',];
+let notesTitles = ['Task 1', 'Task 2',];
 
-let arhivhNotes = [];
-let arhivhNotesTitkles = [];
+let archivedNotes = [];
+let archivedTitles = [];
 
 
-let trachNotes = [];
-let trachNotTitkles = [];
+let trashNotes  = [];
+let trashNoteTitles = [];
 
 function init() {
     getFromLocalStorage();
@@ -15,32 +15,30 @@ function init() {
 
 function saveToLocalStorage() {
     localStorage.setItem("notes", JSON.stringify(notes));
-    localStorage.setItem("notesTitle", JSON.stringify(notesTitle));
+    localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
 
-    localStorage.setItem("arhivhNotes", JSON.stringify(arhivhNotes));
-    localStorage.setItem("arhivhNotesTitkles", JSON.stringify(arhivhNotesTitkles));
+    localStorage.setItem("archivedNotes", JSON.stringify(archivedNotes));
+    localStorage.setItem("archivedTitles", JSON.stringify(archivedTitles));
 
 
-    localStorage.setItem("trachNotes", JSON.stringify(trachNotes));
-    localStorage.setItem("trachNotTitkles", JSON.stringify(trachNotTitkles));
+    localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+    localStorage.setItem("trashNoteTitles", JSON.stringify(trashNoteTitles));
 }
 
 function getFromLocalStorage() {
-    if (localStorage.getItem("notes") && localStorage.getItem("notesTitle") && localStorage.getItem("trachNotes") && localStorage.getItem("trachNotTitkles")) {
+    if (localStorage.getItem("notes") && localStorage.getItem("notesTitles") && localStorage.getItem("archivedNotes") && localStorage.getItem("archivedTitles") && localStorage.getItem("trashNotes") && localStorage.getItem("trashNoteTitles")) {
         notes = JSON.parse(localStorage.getItem("notes"));
-        notesTitle = JSON.parse(localStorage.getItem("notesTitle"));
+        notesTitles = JSON.parse(localStorage.getItem("notesTitles"));
 
-        arhivhNotes = JSON.parse(localStorage.getItem("arhivhNotes"))
-        arhivhNotesTitkles = JSON.parse(localStorage.getItem("arhivhNotesTitkles"))
+        archivedNotes = JSON.parse(localStorage.getItem("archivedNotes"))
+        archivedTitles = JSON.parse(localStorage.getItem("archivedTitles"))
 
 
 
-        trachNotes = JSON.parse(localStorage.getItem("trachNotes"))
-        trachNotTitkles = JSON.parse(localStorage.getItem("trachNotTitkles"))
+        trashNotes = JSON.parse(localStorage.getItem("trashNotes"))
+        trashNoteTitles = JSON.parse(localStorage.getItem("trashNoteTitles"))
     }
 }
-
-
 
 function renderNotes() {
     let contentRef = document.getElementById('content')
@@ -57,10 +55,10 @@ function addNote() {
     let noteInputRef = document.getElementById('note_input');
 
     let noteTitleInput = notesTitleInputRef.value;
-    let noteINput = noteInputRef.value;
+    let noteInput = noteInputRef.value;
 
-    notesTitle.push(noteTitleInput);
-    notes.push(noteINput);
+    notesTitles.push(noteTitleInput);
+    notes.push(noteInput);
 
     renderNotes();
     saveToLocalStorage()
@@ -68,58 +66,48 @@ function addNote() {
     noteInputRef.value = ``;
 }
 
-function achiveNotes(indexNote) {
-    let archivedNote = notes.splice(indexNote, 1)[0];
-    arhivhNotes.push(archivedNote);
+function archivedNote(indexAchivedNote) {
+    let archivedNote = notes.splice(indexAchivedNote, 1)[0];
+    archivedNotes.push(archivedNote);
 
-    let archivedTitle = notesTitle.splice(indexNote, 1)[0];
-    arhivhNotesTitkles.push(archivedTitle);
+    let archivedTitle = notesTitles.splice(indexAchivedNote, 1)[0];
+    archivedTitles.push(archivedTitle);
     saveToLocalStorage()
     renderNotes();
 }
 
-function trach_notes(indexNote) {
-    let trachNot = notes.splice(indexNote, 1)[0];
-    trachNotes.push(trachNot);
 
-    let trachNotTitkle = notesTitle.splice(indexNote, 1)[0];
-    trachNotTitkles.push(trachNotTitkle);
+function trashNotee(indexTrash) {
+    let trashNote = notes.splice(indexTrash, 1)[0];
+    trashNotes.push(trashNote);
+
+    let trashNoteTitle = notesTitles.splice(indexTrash, 1)[0];
+    trashNoteTitles.push(trashNoteTitle);
 
     saveToLocalStorage()
     renderNotes();
 }
 
-function archiveToTrash(index) {
-    let arhivhNotes = arhivhNotes.splice(index, 1)[0];
-    let trashedTitle = arhivhNotesTitkles.splice(index, 1)[0];
-
-    arhivhNotes.push(arhivhNotes);
-    trachNotTitkles.push(arhivhNotesTitkles);
-
-    saveToLocalStorage();
-    openDialogArchive();
-}
-
-function deletetNotesComplet(indexTrachNote) {
-    trachNotes.splice(indexTrachNote, 1);
-    trachNotTitkles.splice(indexTrachNote, 1);
+function deleteNoteComplete(indexNote) {
+    trashNotes.splice(indexNote, 1);
+    trashNoteTitles.splice(indexNote, 1);
 
     saveToLocalStorage()
-    openDialogTrach();
+    openDialogTrash();
 }
 
 
-function openDialogTrach() {
-    let dialogTrach = document.getElementById('trach_dialog');
+function openDialogTrash() {
+    let dialogTrach = document.getElementById('trsh_dialog');
     if (dialogTrach) {
         dialogTrach.showModal();
 
         let dialogTrachRef = document.getElementById('trach_notes_list');
         dialogTrachRef.innerHTML = "";
 
-        for (let i = 0; i < trachNotes.length; i++) {
+        for (let i = 0; i < trashNotes.length; i++) {
             dialogTrachRef.innerHTML += ` 
-            <p> Title: ${trachNotTitkles[i]} => ${trachNotes[i]} </p>
+            <p> Headline: ${trashNoteTitles[i]} => ${trashNotes[i]} </p>
             <button class="delet_btn" onclick="deletetNotesComplet(${i})">X</button></p>
             `
         }
@@ -129,7 +117,7 @@ function openDialogTrach() {
     }
 }
 
-function openDialogArchive() {
+function openDialogArchived() {
     let dialogAchive = document.getElementById('archive_dialog');
     if (dialogAchive) {
         dialogAchive.showModal();
@@ -137,9 +125,9 @@ function openDialogArchive() {
         let dialogAchiveRef = document.getElementById('archive_notes_list');
         dialogAchiveRef.innerHTML = "";
 
-        for (let i = 0; i < trachNotes.length; i++) {
+        for (let i = 0; i < archivedNotes.length; i++) {
             dialogAchiveRef.innerHTML += ` 
-            <p> Title: ${trachNotTitkles[i]} => ${trachNotes[i]} </p>
+            <p> Headline: ${archivedTitles[i]} => ${archivedNotes[i]} </p>
             <button class="delet_btn" onclick="moveArchiveToTrash(${index})">X</button></p>
             `
         }
--- Notitzblock/script/template.js
+++ Notitzblock/script/template.js
@@ -1,31 +1,32 @@
 function gehtNoteTemp(indexNote) {
     return `
     <div class="notes_container">
-    <p class="notes_title">  Title: ${notesTitle[indexNote]} </p>
+    <p class="notes_title">  Headline: ${notesTitles[indexNote]} </p>
     <p class="notes_decpriton"> Beschreibung: ${notes[indexNote]} </p>
     
     <div class="div_btn">
-    <button class="notes_btn" onclick="trach_notes(${indexNote})">Papierkorb</button>
-    <button class="notes_btn" onclick="(})">Achive</button>
+    <button class="notes_btn" onclick="trashNotee(${indexTrash})">Papierkorb</button>
+    <button class="notes_btn" onclick="archivedNote(${indexAchivedNote})">Archived</button>
     </div>
     </div>
     `
 }
 
-function gehtAArchiveNoteTemp(indexTrachNote) {
+function gehtAArchiveNoteTemp(indexAchivedNote) {
     return `
     <dialog class="dialog_trach">   
-    <p> Title: ${trachNotTitkles[indexTrachNote]} => ${trachNotes[indexTrachNote]} </p>
-    <button class="delet_btn" onclick="deletetNotesComplet(${indexTrachNote})">X</button>
+    <p> Headline: ${archivedTitles[indexAchivedNote]} => ${archivedNotes[indexAchivedNote]} </p>
+    <button class="delet_btn" onclick="deleteNoteComplete(${indexNote})">X</button>
     </dialog>
     `
+
 }
 
-function gehtTrachNoteTemp(indexTrachNote) {
+function gehtTrachNoteTemp(indexTrashNote) {
     return `
     <dialog class="dialog_trach">   
-    <p> Title: ${trachNotTitkles[indexTrachNote]} => ${trachNotes[indexTrachNote]} </p>
-    <button class="delet_btn" onclick="deletetNotesComplet(${indexTrachNote})">X</button>
+    <p> Headline: ${trachNotTitkles[indexTrashNote]} => ${trachNotes[indexTrashNote]} </p>
+    <button class="delet_btn" onclick="deleteNoteComplete(${indexNote})">X</button>
     </dialog>
     `
 }
